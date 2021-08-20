fb74665b674d92b9663ee37a4269daa1b476d706
Uchechukwu Ottah <coderboy.exe@gmail.com>
2021-08-20 03:12:21 +0100

    add print program

---
+++ index.js
@@ -0,0 +1,12 @@
+let x;
+
+function handle_click() {
+    if (x==1) {
+        document.getElementById("show").style.display="none";
+        return x=0;
+    }
+    else {
+        document.getElementById("show").style.display="inline";
+        return x=1
+    }
+} 
